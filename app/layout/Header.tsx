9bb04049858e1d34ab07a21ff02b5270709b655e
LSG <sugyeong9611@gmail.com>
2025-02-25 23:41:39 +0900

    routing

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+"use client";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <div className="py-3 text-7xl flex justify-center align-middle">
+    <div
+      className="py-3 text-7xl flex justify-center align-middle hover:cursor-pointer"
+      onClick={() => {
+        router.push("/");
+      }}
+    >
       BOOKSTORE
     </div>
   );
---
+++ app/pages/BookInfoPage.tsx
@@ -0,0 +1,9 @@
+const BookInfoPage = () => {
+  return (
+    <>
+      <button>책 상세 페이지</button>
+    </>
+  );
+};
+
+export default BookInfoPage;
