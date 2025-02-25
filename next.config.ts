7c3dc1123fe8bb8d0cf55581eaa7ddbca7a7d3cc
LSG <sugyeong9611@gmail.com>
2025-02-25 21:48:35 +0900

    page

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   // reactStricMode: true,
   output: "export",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://Sugyeong97.github.io/rgt-task"
+      : "",
 };
 
 export default nextConfig;
